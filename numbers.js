034644e40ac5b46cdfe03b03da4b94ad4b900fa0
Rong <rzheng@fau.edu>
2018-06-08 02:12:04 -0400

    Update numbers.js

--- numbers.js
+++ numbers.js
@@ -1,9 +1,18 @@
-var i=0;
-for (i=0;i<=10;i++)
-{if (i==3)
+var canvas = document.getElementById("testCanvas");
+var ctx = canvas.getContext("2d");
+var w = canvas.attributes.width.value;
+var h = canvas.attributes.height.value;
+ctx.fillStyle = "rgb(0,0,0)";
+ctx.fillRect(0, 0, w, h);
+var imgdata = ctx.getImageData(0, 0, w, h);
+var pixels = imgdata.data;
+var i = 0;
+for (var y = 0; y < h; y++)
+    for (var x = 0; x < w; x++)
     {
-    continue;
+        pixels[i++] = x / w * 255;
+        pixels[i++] = y / h * 255;
+        pixels[i++] = 0;
+        pixels[i++] = 255;
     }
-document.write("The number is " + i);
-document.write("<br/");
-}
+ctx.putImageData(imgdata, 0, 0);
